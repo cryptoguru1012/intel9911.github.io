2b5e0524e196e36b6c21a772c2bc434b0bbd6c65
intel9911 <tiop08317@gmail.com>
2021-12-05 13:44:12 -0800

    first commit with only mint

---
+++ README.md
@@ -0,0 +1,5 @@
+# I Am Crypto Gang - Official NFTs & Community
+
+The I Am Crypto Gang project is designed to let users mint nfts on solana.
+
+It works on devent for now.
---
+++ src/Home.tsx
@@ -0,0 +1,299 @@
+import "./Home.css";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import Countdown from "react-countdown";
+import { Button, CircularProgress, Snackbar } from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
+
+import * as anchor from "@project-serum/anchor";
+
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+import { useAnchorWallet } from "@solana/wallet-adapter-react";
+import { WalletDialogButton } from "@solana/wallet-adapter-material-ui";
+
+import {
+  CandyMachine,
+  awaitTransactionSignatureConfirmation,
+  getCandyMachineState,
+  mintOneToken,
+  shortenAddress,
+} from "./candy-machine";
+
+const ConnectButton = styled(WalletDialogButton)``;
+
+const Wallet = styled.div`
+  background-color: rgb(50, 50, 150);
+  padding: 10px 20px;
+`;
+
+const CounterText = styled.span``; // add your styles here
+
+const HeaderContainer = styled.div`
+  position: fixed;
+  z-index: 2;
+  top: 0;
+  left: 0;
+  height: 70px;
+  width: 100vw;
+  background-color: rgba(255, 255, 255, 0.2);
+  color: rgb(255, 255, 255);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const LeftHeader = styled.div`
+  padding-left: 10px
+`
+
+const RightHeader = styled.div`
+  padding-right: 10px
+`
+
+const MintContainer = styled.div`
+  position: absolute;
+  z-index: 2;
+  top: 120px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: rgba(255, 255, 255, 0.7);
+  color: rgb(0, 0, 0);
+  padding: 10px 150px;
+  border-radius: 10px;
+  font-size: 20px;
+  text-align: center;
+  p {
+    text-align: left;
+  }
+`;
+
+const MintButton = styled(Button)``;
+
+const Background = styled.div`
+  position: relative;
+  height: 100vh
+`;
+
+export interface HomeProps {
+  candyMachineId: anchor.web3.PublicKey;
+  config: anchor.web3.PublicKey;
+  connection: anchor.web3.Connection;
+  startDate: number;
+  treasury: anchor.web3.PublicKey;
+  txTimeout: number;
+}
+
+const Home = (props: HomeProps) => {
+  const [balance, setBalance] = useState<number>();
+  const [isActive, setIsActive] = useState(false); // true when countdown completes
+  const [isSoldOut, setIsSoldOut] = useState(false); // true when items remaining is zero
+  const [isMinting, setIsMinting] = useState(false); // true when user got to press MINT
+
+  const [itemsAvailable, setItemsAvailable] = useState(0);
+  const [itemsRedeemed, setItemsRedeemed] = useState(0);
+  const [itemsRemaining, setItemsRemaining] = useState(0);
+
+  const [alertState, setAlertState] = useState<AlertState>({
+    open: false,
+    message: "",
+    severity: undefined,
+  });
+
+  const [startDate, setStartDate] = useState(new Date(props.startDate));
+
+  const wallet = useAnchorWallet();
+  const [candyMachine, setCandyMachine] = useState<CandyMachine>();
+
+  const refreshCandyMachineState = () => {
+    (async () => {
+      if (!wallet) return;
+
+      const {
+        candyMachine,
+        goLiveDate,
+        itemsAvailable,
+        itemsRemaining,
+        itemsRedeemed,
+      } = await getCandyMachineState(
+        wallet as anchor.Wallet,
+        props.candyMachineId,
+        props.connection
+      );
+
+      const all = await getCandyMachineState(
+        wallet as anchor.Wallet,
+        props.candyMachineId,
+        props.connection
+      );
+
+      setItemsAvailable(itemsAvailable);
+      setItemsRemaining(itemsRemaining);
+      setItemsRedeemed(itemsRedeemed);
+
+      setIsSoldOut(itemsRemaining === 0);
+      setStartDate(goLiveDate);
+      setCandyMachine(candyMachine);
+    })();
+  };
+
+  const onMint = async () => {
+    try {
+      setIsMinting(true);
+      if (wallet && candyMachine?.program) {
+        const mintTxId = await mintOneToken(
+          candyMachine,
+          props.config,
+          wallet.publicKey,
+          props.treasury
+        );
+
+        const status = await awaitTransactionSignatureConfirmation(
+          mintTxId,
+          props.txTimeout,
+          props.connection,
+          "singleGossip",
+          false
+        );
+
+        if (!status?.err) {
+          setAlertState({
+            open: true,
+            message: "Congratulations! Mint succeeded!",
+            severity: "success",
+          });
+        } else {
+          setAlertState({
+            open: true,
+            message: "Mint failed! Please try again!",
+            severity: "error",
+          });
+        }
+      }
+    } catch (error: any) {
+      // TODO: blech:
+      let message = error.msg || "Minting failed! Please try again!";
+      if (!error.msg) {
+        if (error.message.indexOf("0x138")) {
+        } else if (error.message.indexOf("0x137")) {
+          message = `SOLD OUT!`;
+        } else if (error.message.indexOf("0x135")) {
+          message = `Insufficient funds to mint. Please fund your wallet.`;
+        }
+      } else {
+        if (error.code === 311) {
+          message = `SOLD OUT!`;
+          setIsSoldOut(true);
+        } else if (error.code === 312) {
+          message = `Minting period hasn't started yet.`;
+        }
+      }
+
+      setAlertState({
+        open: true,
+        message,
+        severity: "error",
+      });
+    } finally {
+      if (wallet) {
+        const balance = await props.connection.getBalance(wallet.publicKey);
+        setBalance(balance / LAMPORTS_PER_SOL);
+      }
+      setIsMinting(false);
+      refreshCandyMachineState();
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      if (wallet) {
+        const balance = await props.connection.getBalance(wallet.publicKey);
+        setBalance(balance / LAMPORTS_PER_SOL);
+      }
+    })();
+  }, [wallet, props.connection]);
+
+  useEffect(refreshCandyMachineState, [
+    wallet,
+    props.candyMachineId,
+    props.connection,
+  ]);
+
+  return (
+    <main>
+      <Background>
+        <img src="moon_night_stars.jpg" className="back_image" />
+        <img src="CryptoGangs.png" className="gang_image" />
+      </Background>
+      <HeaderContainer>
+        <LeftHeader>
+          <img src="CryptoGangLogo.png" width="50px" />
+        </LeftHeader>
+        <RightHeader>
+        {!wallet ? (
+          <ConnectButton>Connect Wallet</ConnectButton>
+        ) : (
+          <Wallet>{shortenAddress(wallet.publicKey.toBase58() || "")}</Wallet>
+        )}
+        </RightHeader>
+      </HeaderContainer>
+      {wallet && <MintContainer>
+        <p>Balance: {(balance || 0).toLocaleString()} SOL</p>
+        <p>Total Available: {itemsAvailable}</p>
+        <p>Redeemed: {itemsRedeemed}</p>
+        <p>Remaining: {itemsRemaining}</p>
+        <MintButton
+          disabled={isSoldOut || isMinting || !isActive}
+          onClick={onMint}
+          variant="contained"
+        >
+          {isSoldOut ? (
+            "SOLD OUT"
+          ) : isActive ? (
+            isMinting ? (
+              <CircularProgress />
+            ) : (
+              "MINT"
+            )
+          ) : (
+            <Countdown
+              date={startDate}
+              onMount={({ completed }) => completed && setIsActive(true)}
+              onComplete={() => setIsActive(true)}
+              renderer={renderCounter}
+            />
+          )}
+        </MintButton>
+      </MintContainer>}
+
+      <Snackbar
+        open={alertState.open}
+        autoHideDuration={6000}
+        onClose={() => setAlertState({ ...alertState, open: false })}
+      >
+        <Alert
+          onClose={() => setAlertState({ ...alertState, open: false })}
+          severity={alertState.severity}
+        >
+          {alertState.message}
+        </Alert>
+      </Snackbar>
+    </main>
+  );
+};
+
+interface AlertState {
+  open: boolean;
+  message: string;
+  severity: "success" | "info" | "warning" | "error" | undefined;
+}
+
+const renderCounter = ({ days, hours, minutes, seconds, completed }: any) => {
+  return (
+    <CounterText>
+      {hours + (days || 0) * 24} hours, {minutes} minutes, {seconds} seconds
+    </CounterText>
+  );
+};
+
+export default Home;
